578ebfb89f3a1d78594a7d4c2723f7a3e337c38a
Kim Leibowitz <kim@brightlightwellness.com>
2020-10-20 09:33:25 -0400

    views updated GET POST

--- notesapp/notes/views.py
+++ notesapp/notes/views.py
@@ -7,4 +7,71 @@ def notes_list(request):
 
 
 def notes_detail(request, pk):
-    pass
+    note = get_object_or_404(Contact, pk=pk)
+    note = Note.objects.filter(contact=contact)
+
+    if note:
+        note = note[0]
+
+    else:
+        note = None
+
+    return render(request, "contacts/detail_contact.html", {"contact": contact, "note": note})
+
+
+def add_note(request):
+    if request.method == 'GET':
+        form = NoteForm()
+
+    else:
+        form = NoteForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_contacts')
+
+    return render(request, "notes/add_note.html", {"form": form})
+
+
+def edit_note(request, pk):
+    note = get_object_or_404(Contact, pk=pk)
+    if request.method == 'GET':
+        form = NoteForm(instance=note)
+    else:
+        form = NoteForm(data=request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect(to='notes_list')
+
+    return render(request, "notes/edit_note.html", {
+        "form": form,
+        "contact": contact
+    })
+
+
+def delete_note(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect(to='notes_list')
+
+    return render(request, "notes/delete_note.html",
+                  {"note": note})
+
+
+def add_note(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+
+    if request.method == 'GET':
+        form = NoteForm()
+
+    else:
+        form = NoteForm(data=request.POST)
+
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.notes = note
+
+            note.save()
+            return redirect(to='list_contacts')
+
+    return render(request, "notes/add_note.html", {"form": form, "note": note})
